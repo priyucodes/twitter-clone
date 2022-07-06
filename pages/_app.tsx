f1fdf1505a6b5d509852abef247b8030afee3f30
Priyanshu Bhandari <technopriyu@gmail.com>
2022-07-06 22:18:27 +0530

    added session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,13 @@ import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  console.log(session || {});
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
